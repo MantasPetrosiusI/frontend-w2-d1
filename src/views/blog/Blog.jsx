b96c12441e39e307b9f71574e71b5098e6e88d28
MantasPetrosiusI <mantas.petrosius@outlook.com>
2023-03-08 15:35:35 +0000

    6

--- src/views/blog/Blog.jsx
+++ src/views/blog/Blog.jsx
@@ -1,76 +1,347 @@
+import { format, parseISO } from "date-fns";
 import React, { useEffect, useState } from "react";
-import { Container, Image } from "react-bootstrap";
-import { useNavigate, useParams } from "react-router-dom";
+import {
+  Button,
+  Container,
+  Form,
+  Image,
+  ListGroup,
+  Modal,
+} from "react-bootstrap";
+import { Download } from "react-bootstrap-icons";
+import { useParams } from "react-router-dom";
 import BlogAuthor from "../../components/blog/blog-author/BlogAuthor";
 import BlogLike from "../../components/likes/BlogLike";
-import posts from "../../data/posts.json";
 import "./styles.css";
 
 const Blog = (props) => {
-  const [blog, setBlog] = useState([]);
+  const [blog, setBlog] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [file, setFile] = useState("");
+  const [comments, setComments] = useState([]);
+  const [commentToPost, setCommentToPost] = useState("");
+  const [author, setAuthor] = useState("");
   const params = useParams();
-  const navigate = useNavigate();
 
-  const url = process.env.REACT_APP_BE_PROD_URL;
+  const [commentToEdit, setCommentToEdit] = useState(null);
 
-  const { id } = params;
-  const getBlogPosts = async () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = async (id) => {
     try {
-      const res = await fetch(url + "/blogPosts");
-      if (res.ok) {
-        const blogPostsData = await res.json();
-        const blog = blogPostsData?.find((post) => post.id === id);
-        if (blog) {
-          setBlog(blog);
-          setLoading(false);
-        } else {
-          navigate("/404");
+      let response = await fetch(
+        `${process.env.REACT_APP_BE_PROD_URL}/blogPosts/${blog._id}/comments/${id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ comment: commentToEdit.comment }),
         }
+      );
+      if (response.ok) {
+        console.log(response);
       } else {
-        console.log("error");
-        navigate("/404");
+        console.log("Error!");
       }
     } catch (error) {
-      console.error(error);
+      console.log(error);
+    }
+    setShow(false);
+  };
+
+  const handleShow = async (id) => {
+    try {
+      let response = await fetch(
+        `${process.env.REACT_APP_BE_PROD_URL}/blogPosts/${blog._id}/comments`
+      );
+      if (response.ok) {
+        let comments = await response.json();
+        setCommentToEdit(comments.find((comment) => comment._id === id));
+      } else {
+        console.log("Error");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setShow(true);
+  };
+
+  const handleImageUpload = async (id) => {
+    try {
+      const formData = new FormData();
+      formData.append("cover", file);
+      let response = await fetch(
+        `${process.env.REACT_APP_BE_PROD_URL}/blogPosts/${id}/uploadCover`,
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+      if (response.ok) {
+        console.log("Yey!");
+      } else {
+        console.log("Try again!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getBlog = async (id) => {
+    try {
+      let response = await fetch(
+        `${process.env.REACT_APP_BE_PROD_URL}/blogPosts/${id}`
+      );
+      if (response.ok) {
+        let actualPost = await response.json();
+        setBlog(actualPost);
+      } else {
+        console.log("Error!!!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getBlogComments = async (id) => {
+    try {
+      let response = await fetch(
+        `${process.env.REACT_APP_BE_PROD_URL}/blogPosts/${id}/comments`
+      );
+      if (response.ok) {
+        let allComments = await response.json();
+        setComments(allComments);
+      } else {
+        console.log("error");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSubmit = async (id) => {
+    try {
+      let response = await fetch(
+        `${process.env.REACT_APP_BE_PROD_URL}/blogPosts/${id}/comments`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            comment: commentToPost,
+            author: author,
+          }),
+        }
+      );
+      if (response.ok) {
+        console.log("OK");
+      } else {
+        console.log("error");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDelete = async (postId, commentId) => {
+    try {
+      await fetch(
+        `${process.env.REACT_APP_BE_PROD_URL}/blogPosts/${postId}/comments/${commentId}`,
+        {
+          method: "DELETE",
+        }
+      );
+    } catch (error) {
+      console.log(error);
     }
   };
 
   useEffect(() => {
-    getBlogPosts();
-    // eslint-disable-next-line
+    const { id } = params;
+    getBlog(id);
+    getBlogComments(id);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useEffect(() => {
+    if (blog) {
+      setLoading(false);
+    }
+  }, [blog]);
+
   if (loading) {
+    return <div>loading</div>;
+  } else {
     return (
-      <div className="blog-details-root">
-        <Container>
-          <Image className="blog-details-cover" src={blog.coverURL} fluid />
-          <h1 className="blog-details-title">{blog.title}</h1>
+      <>
+        {blog && (
+          <div className="blog-details-root">
+            <Container>
+              <Image className="blog-details-cover" src={blog.cover} fluid />
 
-          <div className="blog-details-container">
-            <div className="blog-details-author">
-              <BlogAuthor {...blog.author} />
-            </div>
-            <div className="blog-details-info">
-              <div>{blog.createdAt}</div>
-              <div>{`${blog.readTime?.value} ${blog.readTime?.unit} read`}</div>
-              <div
-                style={{
-                  marginTop: 20,
-                }}
-              >
-                <BlogLike defaultLikes={["123"]} onChange={console.log} />
+              <Form.Group className="d-flex justify-content-center">
+                <Form.Control
+                  type="file"
+                  style={{ maxWidth: "85%" }}
+                  onChange={(e) => {
+                    const files = e.target.files;
+                    if (files && files.length > 0) {
+                      setFile(files[0]);
+                    } else {
+                      setFile(null);
+                    }
+                  }}
+                />
+                <Button
+                  variant="primary"
+                  style={{ marginLeft: "5px" }}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    if (file) {
+                      handleImageUpload(blog._id);
+                    }
+                  }}
+                >
+                  Update
+                </Button>
+                <a
+                  href={`${process.env.REACT_APP_BE_PROD_URL}/blogPosts/${blog._id}/pdf`}
+                  className="btn btn-primary"
+                  style={{ marginLeft: "5px" }}
+                >
+                  <Download /> PDF
+                </a>
+              </Form.Group>
+
+              <h1 className="blog-details-title">{blog.title}</h1>
+
+              <div className="blog-details-container">
+                <div className="blog-details-author">
+                  <BlogAuthor {...blog.author} />
+                </div>
+                <div className="blog-details-info">
+                  <div>
+                    {format(parseISO(blog.createdAt.toString()), "dd MMM yyyy")}
+                  </div>
+                  <div>{`${blog.readTime.value} ${blog.readTime.unit} read`}</div>
+                  <div
+                    style={{
+                      marginTop: 20,
+                    }}
+                  >
+                    <BlogLike defaultLikes={["123"]} />
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
 
-          <div
-            dangerouslySetInnerHTML={{
-              __html: blog.content,
-            }}
-          ></div>
-        </Container>
-      </div>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: blog.content,
+                }}
+              ></div>
+              {comments && (
+                <ListGroup>
+                  {comments.map((comment) => {
+                    return (
+                      <ListGroup.Item key={comment._id}>
+                        <div className="d-flex">
+                          <div
+                            className="d-flex flex-column"
+                            style={{ marginRight: "auto" }}
+                          >
+                            <div>{comment.comment}</div>
+                            <div style={{ fontSize: "14px" }}>
+                              - {comment.author}
+                            </div>
+                          </div>
+                          <div className="d-flex">
+                            <Button
+                              className="mt-2"
+                              style={{ marginRight: "5px" }}
+                              variant="outline-primary"
+                              onClick={() => {
+                                handleShow(comment._id);
+                              }}
+                            >
+                              Edit
+                            </Button>
+                            <Button
+                              className="mt-2"
+                              variant="danger"
+                              onClick={(e) => {
+                                e.preventDefault();
+                                handleDelete(blog._id, comment._id);
+                              }}
+                            >
+                              Delete
+                            </Button>
+                          </div>
+                        </div>
+                      </ListGroup.Item>
+                    );
+                  })}
+                </ListGroup>
+              )}
+              <Form.Group controlId="blog-form" className="mt-3">
+                <Form.Control
+                  className="mt-2"
+                  size="lg"
+                  placeholder="Add a comment"
+                  value={commentToPost}
+                  onChange={(e) => setCommentToPost(e.target.value)}
+                />
+                <Form.Control
+                  className="mt-2"
+                  size="lg"
+                  placeholder="Your name"
+                  value={author}
+                  onChange={(e) => setAuthor(e.target.value)}
+                />
+                <Button
+                  className="mt-2"
+                  variant="primary"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleSubmit(blog._id);
+                  }}
+                >
+                  Submit
+                </Button>
+              </Form.Group>
+            </Container>
+          </div>
+        )}
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form.Control
+              as="textarea"
+              rows={5}
+              value={commentToEdit ? commentToEdit.comment : ""}
+              onChange={(e) => {
+                setCommentToEdit({
+                  ...commentToEdit,
+                  comment: e.target.value,
+                });
+              }}
+            />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              variant="primary"
+              onClick={() => {
+                handleClose(commentToEdit._id);
+              }}
+            >
+              Submit
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
     );
   }
 };
--- src/views/new/New.jsx
+++ src/views/new/New.jsx
@@ -15,93 +15,209 @@ const NewBlogPost = (props) => {
     setHTML(html);
   }, [editorState]);
 
-  const url = process.env.REACT_APP_BE_PROD_URL;
-  const [title, setTitle] = useState(null);
-  const [category, setCategory] = useState(null);
-  const [authorName, setAuthorName] = useState(null);
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("Technology");
+  const [coverFile, setCoverFile] = useState("");
+  const [time, setTime] = useState(1);
+  const [timeUnit, setTimeUnit] = useState("minute");
+  const [authorName, setAuthorName] = useState("");
+  const [authorSurname, setAuthorSurname] = useState("");
+  const [authorAvatar, setAuthorAvatar] = useState("");
 
-  const publishNewBlogPost = async () => {
+  const [blogPost, setBlogPost] = useState(null);
+
+  const getAuthor = async () => {
     try {
-      const newBlogPost = {
-        title: title,
-        category: category,
-        content: html,
-        cover: "",
-        readTime: {
-          value: 3,
-          unit: "minutes",
-        },
-        author: {
-          name: authorName,
-          id: "123456",
-        },
-      };
-
-      const res = await fetch(url + "/blogPosts", {
-        method: "POST",
-        body: JSON.stringify(newBlogPost),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      if (res.ok) {
-        setHTML("");
-        setTitle("");
-        setCategory("Science");
+      let response = await fetch(
+        `${process.env.REACT_APP_BE_PROD_URL}/authors`
+      );
+      if (response.ok) {
+        let authors = await response.json();
+        let author = authors.find(
+          (author) =>
+            author.name.toLowerCase() === authorName.toLowerCase() &&
+            author.surname.toLowerCase() === authorSurname.toLowerCase()
+        );
+        console.log(author);
+        if (author) {
+          console.log(author.avatar);
+          setAuthorAvatar(author.avatar);
+        } else {
+          setAuthorAvatar(
+            `https://ui-avatars.com/api/?name=${authorName}+${authorSurname}`
+          );
+        }
       } else {
-        console.log("Error!");
+        console.log("error");
       }
     } catch (error) {
-      console.error(error);
+      console.log(error);
     }
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    publishNewBlogPost();
+  const handleCoverUpload = async (id) => {
+    try {
+      const formData = new FormData();
+      formData.append("cover", coverFile);
+      let response = await fetch(
+        `${process.env.REACT_APP_BE_PROD_URL}/blogPosts/${id}/uploadCover`,
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+      if (response.ok) {
+        console.log("Yey!");
+      } else {
+        console.log("Try again!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (coverFile) {
+      handleCoverUpload(blogPost._id);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [blogPost]);
+
+  useEffect(() => {
+    if (authorAvatar) {
+      sendPost();
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [authorAvatar]);
+
+  const sendPost = async () => {
+    try {
+      let newPost = {
+        category: category,
+        title: title,
+        cover: "https://picsum.photos/800/400",
+        readTime: {
+          value: time,
+          unit: timeUnit,
+        },
+        author: {
+          name: `${authorName.toUpperCase()} ${authorSurname.toUpperCase()}`,
+          avatar: authorAvatar,
+        },
+        content: html,
+      };
+      let response = await fetch(
+        `${process.env.REACT_APP_BE_PROD_URL}/blogPosts`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newPost),
+        }
+      );
+      if (response.ok) {
+        let post = await response.json();
+        setBlogPost(post);
+        console.log(authorAvatar);
+      } else {
+        console.log("Error!!!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <Container className="new-blog-container">
-      <Form className="mt-5" onSubmit={handleSubmit}>
-        <Form.Group
-          controlId="blog-form"
-          className="mt-3"
-          value={authorName}
-          onChange={(e) => {
-            setAuthorName(e.target.value);
-          }}
-        >
-          <Form.Label>Author</Form.Label>
-          <Form.Control size="lg" placeholder="Author" />
-        </Form.Group>
-        <Form.Group
-          controlId="blog-form"
-          className="mt-3"
-          value={title}
-          onChange={(e) => {
-            setTitle(e.target.value);
-          }}
-        >
+      <Form
+        className="mt-5"
+        onSubmit={async (e) => {
+          e.preventDefault();
+          getAuthor();
+        }}
+      >
+        <Form.Group controlId="blog-form" className="mt-3">
           <Form.Label>Title</Form.Label>
-          <Form.Control size="lg" placeholder="Title" />
+          <Form.Control
+            size="lg"
+            placeholder="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </Form.Group>
-        <Form.Group
-          controlId="blog-category"
-          className="mt-3"
-          value={category}
-          onChange={(e) => {
-            setCategory(e.target.value);
-          }}
-        >
+        <Form.Group controlId="blog-category" className="mt-3">
           <Form.Label>Category</Form.Label>
-          <Form.Control size="lg" as="select">
-            <option>Science</option>
+          <Form.Control
+            size="lg"
+            as="select"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
             <option>Technology</option>
-            <option>Health</option>
-            <option>Daily Life</option>
-            <option>Space</option>
+            <option>History</option>
+            <option>Drama</option>
+            <option>Mistery</option>
+            <option>News</option>
           </Form.Control>
+        </Form.Group>
+        <Form.Group className="mt-3">
+          <Form.Label>Cover Image</Form.Label>
+          <Form.Control
+            type="file"
+            onChange={(e) => {
+              const files = e.target.files;
+              if (files && files.length > 0) {
+                setCoverFile(files[0]);
+              } else {
+                setCoverFile(null);
+              }
+            }}
+          />
+        </Form.Group>
+        <Form.Group className="mt-3">
+          <Form.Label>Time to read</Form.Label>
+          <Form.Control
+            type="number"
+            value={time}
+            onChange={(e) => {
+              setTime(e.target.value);
+            }}
+          />
+        </Form.Group>
+        <Form.Group className="mt-3">
+          <Form.Label>Time unit</Form.Label>
+          <Form.Control
+            size="lg"
+            as="select"
+            value={timeUnit}
+            onChange={(e) => setTimeUnit(e.target.value)}
+          >
+            <option>minute</option>
+            <option>hour</option>
+            <option>day</option>
+          </Form.Control>
+        </Form.Group>
+        <Form.Group className="mt-3">
+          <Form.Label>Author Name</Form.Label>
+          <Form.Control
+            type="text"
+            value={authorName}
+            onChange={(e) => {
+              setAuthorName(e.target.value);
+            }}
+          />
+        </Form.Group>
+        <Form.Group className="mt-3">
+          <Form.Label>Author Surname</Form.Label>
+          <Form.Control
+            type="text"
+            value={authorSurname}
+            onChange={(e) => {
+              setAuthorSurname(e.target.value);
+            }}
+          />
         </Form.Group>
         <Form.Group controlId="blog-content" className="mt-3">
           <Form.Label>Blog Content</Form.Label>
@@ -111,7 +227,6 @@ const NewBlogPost = (props) => {
             wrapperClassName="wrapperClassName"
             editorClassName="editorClassName"
             onEditorStateChange={setEditorState}
-            value={html}
           />
         </Form.Group>
         <Form.Group className="d-flex mt-3 justify-content-end">
